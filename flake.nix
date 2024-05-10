2d73020d8ae76812794cfb35972b01d332febdc6
Demelary <9827644+Firenezz@users.noreply.github.com>
2024-05-10 11:53:17 -0400

    Add workflows

--- flake.nix
+++ flake.nix
@@ -5,13 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, systems, flake-utils, ... }:
+  outputs = { self, nixpkgs, systems, flake-utils, flake-parts, ... }:
     let
       eachSystem = nixpkgs.lib.genAttrs (import systems);
     in {
-      packages.riscv-toolchain = eachSystem (system: {
+      packages = eachSystem (system: {
         system = import nixpkgs {            
           localSystem = system;
           crossSystem = {
